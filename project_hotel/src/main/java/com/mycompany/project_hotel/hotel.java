7858675f68fa9797a0cfd159729d06bdb6b5a059
harris faqih <harrisfaqih42031@mhs.unida.gontor.ac.id>
2022-10-11 12:52:39 +0700

    add hotel class

---
+++ project_hotel/src/main/java/com/mycompany/project_hotel/hotel.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.project_hotel;
+
+/**
+ *
+ * @author My PC
+ */
+public class hotel {
+    public static void main(String[] args) {
+        
+    }
+}
